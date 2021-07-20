edc482a865dcb90a608bf05876ead9c7a59fa2b3
Nader Gharibian Fard <nader.gharibian@outlook.com>
2021-07-20 11:48:20 +0430

    clone

---
+++ WebSocket/CSharp/WebSocket.Broadcast/WebSocket.Broadcast/Domain/SocketTrack.cs
@@ -0,0 +1,8 @@
+﻿namespace WebSocket.Broadcast.Domain
+{
+    public class SocketTrack
+    {
+        public System.Net.WebSockets.WebSocket Socket { get; set; }
+        public string  Party { get; set; }
+    }
+}
---
+++ WebSocket/CSharp/WebSocket.Broadcast/WebSocket.Broadcast/Handlers/WebSocketMessageHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+using WebSocket.Broadcast.SocketManager;
+
+namespace WebSocket.Broadcast.Handlers
+{
+    public class WebSocketMessageHandler : SocketHandler
+    {
+        public WebSocketMessageHandler(ConnectionManager connections) : base(connections)
+        {
+
+        }
+        public override async Task OnConnected(System.Net.WebSockets.WebSocket socket, string party)
+        {
+            await base.OnConnected(socket,party);
+            var socketID = Connections.GetID(socket);
+            await BroadcastParty(party, $"{socketID} just joined the party " + party);
+        }
+        public override  async Task Receive( string party, System.Net.WebSockets.WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
+        {
+            var socketID = Connections.GetID(socket);
+            var message = $"{socketID} said: {Encoding.UTF8.GetString(buffer, 0, result.Count)}";
+            await BroadcastParty(party, message);// SendByIDMessage(socketID, message)
+                 
+
+        }
+    }
+}
